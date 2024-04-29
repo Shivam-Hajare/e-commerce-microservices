62bd78186db70e7ae979af16b82e47919ad644b4
Shivam Hajare <shivamhajare96@gmail.com>
2024-04-29 08:45:33 +0530

    first comment

---
+++ ServiceRegistrationServer/src/test/java/com/app/ServiceRegistrationServer/ServiceRegistrationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.ServiceRegistrationServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistrationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
